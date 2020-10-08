b9e2c3f7f5cf908bf7da358c625158d57719cdf8
Yaniddze <FaceCrack1337@gmail.com>
2020-10-09 03:58:06 +0500

    :pencil2: Create module

---
+++ src/model/index.ts
@@ -0,0 +1,5 @@
+export { SimonButton } from './SimonButton';
+export { CompareSequences } from './CompareSequences';
+export { DifficultLevels } from './DifficultLevels';
+export { GenerateSequence } from './GenerateSequence';
+export { PlayRound } from './PlayRound';
